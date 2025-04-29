a878de1d07de0aec1e91bbe26f9964ba02d25045
Kunal Shrivastav <54164834+poke-key@users.noreply.github.com>
2025-04-29 12:16:46 -0700

    added container component

---
+++ src/components/ui/container.tsx
@@ -0,0 +1,16 @@
+// src/components/ui/container.tsx
+import { cn } from "../../lib/utils"
+
+interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function Container({ className, ...props }: ContainerProps) {
+  return (
+    <div
+      className={cn(
+        "mx-auto max-w-7xl px-4 sm:px-6 lg:px-8",
+        className
+      )}
+      {...props}
+    />
+  );
+}
