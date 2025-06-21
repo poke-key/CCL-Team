ad7fbc0852208902bfc7d3eaf915205f87506537
Kunal Shrivastav <54164834+poke-key@users.noreply.github.com>
2025-06-21 00:23:28 -0700

    fixed warning in match summary

--- src/components/MatchSummary.tsx
+++ src/components/MatchSummary.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Game } from './types';
 import { getMatchResults, calculateMatchScore, calculateDeficit, getPlayerPerformance } from './utils';
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from './ui/table';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './ui/table';
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
 import { Badge } from './ui/badge';
 
