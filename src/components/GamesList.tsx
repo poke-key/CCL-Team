dce6565f193fc7b133f7273c254c21cd4e436660
Kunal Shrivastav <54164834+poke-key@users.noreply.github.com>
2025-09-14 00:26:54 -0700

    undid that change

--- src/components/GamesList.tsx
+++ src/components/GamesList.tsx
@@ -83,7 +83,6 @@ const GamesList: React.FC<GamesListProps> = ({
                       size="lg"
                       onClick={() => setSelectedGame(game)}
                       className="bg-blue-600 hover:bg-blue-700 text-white font-semibold text-lg px-6 py-2"
-                      style={{ color: 'white' }}
                     >
                       View Game
                     </Button>
